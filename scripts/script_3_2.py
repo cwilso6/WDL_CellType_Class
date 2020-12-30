720e4b7eb0567bec88310e810c65aaea5d339377
Chris <cwilso6@clemson.edu>
2020-12-30 18:26:53 -0500

    Added AUC to Sect 3.2

--- scripts/script_3_2.py
+++ scripts/script_3_2.py
@@ -2,10 +2,8 @@ import keras
 from keras import regularizers
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
+from sklearn.metrics import roc_auc_score
 
 def gene_weights_naive(model):
 #This function computes the influence of each gene on the model, following the formula (2)
@@ -19,7 +17,7 @@ def gene_weights_naive(model):
 Chang = pd.read_table("data/Chang_small.txt", sep = ' ', index_col = 0)
 #Drop cells types that are not in Tirosh
 Chang = Chang.loc[Chang['CellType'].str.contains("DCs|Plasma_cells|pDCs|Myofibroblasts|Naive|Tumor") == False]
-Chang['CellType'] = cell_type_train.astype('category')
+Chang['CellType'] = Chang['CellType'].astype('category')
 #Recode categories to match those in Tirosh
 mapper = {'B_cells': 'B_cells','CAFs': 'CAFs','CD4_T_cells': 'CD4_T_cells', 'CD8_act_T_cells': 'CD8_T_cells',
           'CD8_ex_T_cells': 'CD8_T_cells', 'CD8_mem_T_cells': 'CD8_T_cells', 'Macrophages': 'Macrophages',
@@ -110,3 +108,47 @@ confusion_model = pd.crosstab(predicted_type,Tirosh['CellType'])
 probs_wdl_model = pd.DataFrame(predicted_prob, columns = Y_train.columns.values, index = Tirosh.index)
 probs_wdl_model['Truth'] = cell_type_test
 probs_wdl_model['predicted'] = predicted_type
+#########################################################################################################
+#Wide component in the first hidden layer
+#model (two input now, features 0:4 to patah A, 2:7 to path B)
+input_A = keras.layers.Input(shape=(np.shape(X_train_A)[1],))
+input_B = keras.layers.Input(shape=(np.shape(X_train_B)[1],))
+hidden1 = keras.layers.Dense(int(first_layer), activation="relu",kernel_regularizer=regularizers.l2(0.01))(input_B)
+dropout1 = keras.layers.Dropout(0.2)(hidden1)
+concat = keras.layers.concatenate([input_A, hidden1])
+hidden2 = keras.layers.Dense(int(second_layer), activation="relu",kernel_regularizer=regularizers.l2(0.01))(concat)
+dropout2 = keras.layers.Dropout(0.2)(hidden2)
+output = keras.layers.Dense(Y_train.shape[1],activation='softmax')(dropout2)
+model_wdl_first = keras.models.Model(inputs=[input_A, input_B], outputs=[output])
+model_wdl_first.summary()
+model_wdl_first.compile(loss='categorical_crossentropy', optimizer="adam",metrics=['accuracy'])
+
+history_wdl_first = model_wdl_first.fit([X_train_A.to_numpy(), X_train_B.to_numpy()], Y_train, epochs=10, batch_size=128)
+
+history_wdl_first_df = pd.DataFrame(history_wdl_first.history)
+
+predicted_prob = model_wdl_first.predict([X_test_A, X_test_B])
+predicted = [np.where(predicted_prob[i] == max(predicted_prob[i]))[0][0] for i in range(predicted_prob.shape[0])]
+predicted_type = [Y_train.columns.values[i] for i in predicted]
+predicted_type = pd.Series(predicted_type, index = Tirosh.index)
+confusion_model = pd.crosstab(predicted_type,Tirosh['CellType'])
+probs_wdl_first_model = pd.DataFrame(predicted_prob, columns = Y_train.columns.values, index = Tirosh.index)
+probs_wdl_first_model['Truth'] = cell_type_test
+probs_wdl_first_model['predicted'] = predicted_type
+###################################################################################################
+#AUC Comparisons
+
+roc_micro = [roc_auc_score(Y_test,probs_model.drop(columns = ['Truth', 'predicted']),'micro'),
+             roc_auc_score(Y_test,probs_wdl_model.drop(columns = ['Truth', 'predicted']),'micro'),
+             roc_auc_score(Y_test,probs_wdl_first_model.drop(columns = ['Truth', 'predicted']),'micro')]
+
+roc_macro = [roc_auc_score(Y_test,probs_model.drop(columns = ['Truth', 'predicted']),'macro'),
+             roc_auc_score(Y_test,probs_wdl_model.drop(columns = ['Truth', 'predicted']),'macro'),
+             roc_auc_score(Y_test,probs_wdl_first_model.drop(columns = ['Truth', 'predicted']),'macro')]
+
+roc = pd.DataFrame(columns = Y_train.columns.values)
+roc.loc['Naive',:] = roc_auc_score(Y_test,probs_model.drop(columns = ['Truth', 'predicted']),None)
+roc.loc['WDL Second Layer',:] = roc_auc_score(Y_test,probs_wdl_model.drop(columns = ['Truth', 'predicted']),None)
+roc.loc['WDL First Layer',:] = roc_auc_score(Y_test,probs_wdl_first_model.drop(columns = ['Truth', 'predicted']),None)
+roc['Micro'] = roc_micro
+roc['Macro'] = roc_macro
