ee047be2f79cd6d30623bb5188a335e1a9515219
Chris <cwilso6@clemson.edu>
2020-12-30 18:03:25 -0500

    Started script for 3.2

--- scripts/script_3_2.py
+++ scripts/script_3_2.py
@@ -17,12 +17,96 @@ def gene_weights_naive(model):
 
 #Importing Chang data and separiating into feature matrix (X) and one hot enconded responses (Y)
 Chang = pd.read_table("data/Chang_small.txt", sep = ' ', index_col = 0)
-cell_type = Chang['CellType']
-Y_train = pd.get_dummies(cell_type)
+#Drop cells types that are not in Tirosh
+Chang = Chang.loc[Chang['CellType'].str.contains("DCs|Plasma_cells|pDCs|Myofibroblasts|Naive|Tumor") == False]
+Chang['CellType'] = cell_type_train.astype('category')
+#Recode categories to match those in Tirosh
+mapper = {'B_cells': 'B_cells','CAFs': 'CAFs','CD4_T_cells': 'CD4_T_cells', 'CD8_act_T_cells': 'CD8_T_cells',
+          'CD8_ex_T_cells': 'CD8_T_cells', 'CD8_mem_T_cells': 'CD8_T_cells', 'Macrophages': 'Macrophages',
+          'Melanocytes':'Melanocytes', 'Tfh': 'CD4_T_cells'}
+Chang['CellType'] = Chang['CellType'].map(mapper).fillna(Chang['CellType'])
+cell_type_train = Chang['CellType'].astype('category')
+
+Y_train = pd.get_dummies(Chang['CellType'])
 X_train = Chang.drop(columns = 'CellType')
 
 #Importing Tirosh data and separiating into feature matrix (X) and one hot enconded responses (Y)
 Tirosh = pd.read_table("data/Tirosh_small.txt", sep = ' ', index_col = 0)
-cell_type = Chang['CellType']
-Y_test = pd.get_dummies(cell_type)
-X_test = Chang.drop(columns = 'CellType')
+cell_type_test = Tirosh['CellType']
+
+Y_test = pd.get_dummies(cell_type_test)
+X_test = Tirosh.drop(columns = 'CellType')
+
+#Z-transformation for the training and test sets, where the mean and variacnes from the
+#training set are used to scale the testing data
+#This might not be appropriate since were orginally on diffeent scale,
+#This will change the correlation stucture in the test set becuase of using
+#potentially misspecified means
+scaler = StandardScaler()
+X_train_val = scaler.fit_transform(X_train.values)
+X_test_val = scaler.transform(X_test.values)
+
+#Setting up arcitecture for hidden layers (number of nodes)
+first_layer = 100
+second_layer = 75
+
+#Naive Model
+input = keras.layers.Input(shape=(np.shape(X_train)[1],))
+hidden1 = keras.layers.Dense(first_layer, activation="relu",kernel_regularizer=regularizers.l2(0.01))(input)
+dropout1 = keras.layers.Dropout(0.2)(hidden1)
+hidden2 = keras.layers.Dense(second_layer, activation="relu",kernel_regularizer=regularizers.l2(0.01))(dropout1)
+dropout2 = keras.layers.Dropout(0.2)(hidden2)
+output = keras.layers.Dense(Y_train.shape[1],activation='softmax')(dropout2)
+model_naive = keras.models.Model(inputs=[input], outputs=[output])
+model_naive.summary()
+model_naive.compile(loss='categorical_crossentropy', optimizer="adam" ,metrics=['accuracy'])
+
+#Train/Fit
+history_naive = model_naive.fit(X_train_val, Y_train, epochs=10, batch_size=128)
+history_naive_df = pd.DataFrame(history_naive.history)
+
+predicted_prob = model_naive.predict(X_test)
+predicted = [np.where(predicted_prob[i] == max(predicted_prob[i]))[0][0] for i in range(predicted_prob.shape[0])]
+predicted_type = [Y_train.columns.values[i] for i in predicted]
+predicted_type = pd.Series(predicted_type, index = Tirosh.index)
+confusion_model = pd.crosstab(predicted_type,Tirosh['CellType'])
+probs_model = pd.DataFrame(predicted_prob, columns = Y_train.columns.values, index = Tirosh.index)
+probs_model['Truth'] = cell_type_test
+probs_model['predicted'] = predicted_type
+##############################################################################################################
+#WDL
+bio_relevant = ['CD4','CD8A', 'CD8B', 'GZMK','MIA', 'PMEL', 'MLANA'] #Presented in the paper
+bio_relevant = list(set(bio_relevant).intersection(set(X_train.columns.values))) #Genes in the subset of data
+#Now split data into all inputs A and B
+X_train_A = X_train[bio_relevant]
+X_test_A = X_test[bio_relevant]
+X_train_B = X_train.drop(columns = bio_relevant)
+X_test_B = X_test.drop(columns = bio_relevant)
+
+#model (two input now, features 0:4 to patah A, 2:7 to path B)
+input_A = keras.layers.Input(shape=(np.shape(X_train_A)[1],))
+input_B = keras.layers.Input(shape=(np.shape(X_train_B)[1],))
+hidden1 = keras.layers.Dense(int(first_layer), activation="relu",kernel_regularizer=regularizers.l2(0.01))(input_B)
+#hidden1 = keras.layers.Dense(int(first_layer), activation="relu")(input_B)
+dropout1 = keras.layers.Dropout(0.2)(hidden1)
+hidden2 = keras.layers.Dense(int(second_layer), activation="relu",kernel_regularizer=regularizers.l2(0.01))(hidden1)
+#hidden2 = keras.layers.Dense(int(second_layer), activation="relu")(hidden1)
+dropout2 = keras.layers.Dropout(0.2)(hidden2)
+concat = keras.layers.concatenate([input_A, hidden2])
+output = keras.layers.Dense(Y_train.shape[1],activation='softmax')(concat)
+model_wdl = keras.models.Model(inputs=[input_A, input_B], outputs=[output])
+model_wdl.summary()
+model_wdl.compile(loss='categorical_crossentropy', optimizer="adam",metrics=['accuracy'])
+
+history_wdl = model_wdl.fit([X_train_A.to_numpy(), X_train_B.to_numpy()], Y_train, epochs=10, batch_size=128)
+
+history_wdl_df = pd.DataFrame(history_wdl.history)
+
+predicted_prob = model_wdl.predict([X_test_A, X_test_B])
+predicted = [np.where(predicted_prob[i] == max(predicted_prob[i]))[0][0] for i in range(predicted_prob.shape[0])]
+predicted_type = [Y_train.columns.values[i] for i in predicted]
+predicted_type = pd.Series(predicted_type, index = Tirosh.index)
+confusion_model = pd.crosstab(predicted_type,Tirosh['CellType'])
+probs_wdl_model = pd.DataFrame(predicted_prob, columns = Y_train.columns.values, index = Tirosh.index)
+probs_wdl_model['Truth'] = cell_type_test
+probs_wdl_model['predicted'] = predicted_type
